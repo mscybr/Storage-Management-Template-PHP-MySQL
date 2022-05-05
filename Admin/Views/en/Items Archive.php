d3472ff8aa2dd79263cd54426780fb979dc2694f
mscybr <52931433+mscybr@users.noreply.github.com>
2022-05-05 17:43:43 +0200

    Update Items Archive.php

--- Admin/Views/en/Items Archive.php
+++ Admin/Views/en/Items Archive.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://bootswatch.com/5/lux/bootstrap.min.css" rel="stylesheet">
 
     <title> Archive </title>
 </head>
